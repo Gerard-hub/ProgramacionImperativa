73db0e7dd053449e393aedf12c2604098cbfaea0
Gerard-hub <gerardetcetera@hotmail.com>
2022-08-11 16:58:39 -0300

    segundo commit

--- Ejer5s/ejer5s.js
+++ Ejer5s/ejer5s.js
@@ -18,14 +18,49 @@ const prompt = require("prompt-sync")({ sigint: true });
 
 // console.log(test1 (10, 40))
 
-function pulgadasACentimetros(pulgadas){
-    return pulgadas * 2.54
-}
+// 1)
+// function pulgadasACentimetros(pulgadas){
+//     return pulgadas * 2.54
+// }
 
-console.log(pulgadasACentimetros(1))
+// console.log(pulgadasACentimetros(1))
 
-function stringAURL (grupo2){
-    return `http://www.${grupo2}.com`
-}
+// 2)
+// function stringAURL (grupo2){
+//     return `http://www.${grupo2}.com`
+// }
 
-console.log(stringAURL("nosotros"))
+// console.log(stringAURL("nosotros"))
+
+// 3)
+// function stringConAdmiracion (frase) {
+//     return `¡${frase}!`
+// }
+
+// console.log(stringConAdmiracion("prueba"))
+
+// 4)
+// function calcEdadPerros (edad) {
+//     return edad * 7
+// }
+
+// Arrow Function version
+// let calcEdadPerros = (edad) => edad * 7
+
+// console.log(calcEdadPerros(2))
+
+// 5) se considera el requerimiento de q el mes de trabajo tiene 40 horas
+// function calcValorHoraSueldo (sueldo) {
+//     return sueldo / 40
+// }
+
+// console.log(calcValorHoraSueldo(40000))
+
+// 6)
+// function calculadorIMC(altura, peso) {
+//     return peso / (altura*altura)
+// }
+
+let calculadorIMC = (altura, peso) => peso / (altura*altura)
+
+console.log(calculadorIMC(1.8, 100))
